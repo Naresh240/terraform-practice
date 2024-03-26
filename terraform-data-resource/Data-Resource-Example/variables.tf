c28ee40ade9d7ed05cdee868628fd3203a1f7adc
Naresh240 <58024415+Naresh240@users.noreply.github.com>
2024-03-26 09:33:09 +0530

    Update variables.tf

--- terraform-data-resource/Data-Resource-Example/variables.tf
+++ terraform-data-resource/Data-Resource-Example/variables.tf
@@ -1,4 +1,19 @@
-variable "ami_id" {
-  type = string
-  default = "ami-04d29b6f966df1537"
+variable "image_id" {
+  type    = string
+  default = "ami-019f9b3318b7155c5"
+}
+
+variable "inst_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "tag_name" {
+  type    = string
+  default = "Helloworld"
+}
+
+variable "number_of_instances" {
+  type    = number
+  default = 1
 }
