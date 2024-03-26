2caac88bb6a04751525a4833851b5626fa860aca
Naresh240 <58024415+Naresh240@users.noreply.github.com>
2024-03-26 09:33:30 +0530

    Update providers.tf

--- terraform-data-resource/Data-Resource-Example/providers.tf
+++ terraform-data-resource/Data-Resource-Example/providers.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region                  = "us-east-1"
-  shared_credentials_file = "/C/Users/hp/.aws/credentials"
+  shared_credentials_files = ["~/.aws/credentials"]
+  region                   = "us-east-2"
+  profile                  = "dev"
 }
